b15f88e7672bdba33dc84c982f99ebc49450977f
İLYAS EFE SARISÜLEYMAN <82322033+ilyasefe4161@users.noreply.github.com>
2024-05-25 22:29:14 +0300

    Add files via upload

---
+++ aboutus.php
@@ -0,0 +1,14 @@
+<table width="1120" align="center" border="0" cellpadding="0" cellspacing="0">
+    <tr height="50"bgcolor="orange">
+        <td align="left"><h2>&nbsp;ABOUT US</h2></td>
+    </tr>
+    <tr height="50">
+        <td align="left" style ="border-bottom: 1px dashed #ccc;">&nbsp;You can get to know our company better.</td>
+    </tr>
+    <tr>
+        <td align="left">&nbsp;</td>
+    </tr>
+    <tr>
+        <td align="left"><?php echo DonusumleriGeriDondur($aboutUsText); ?></td>
+    </tr>
+</table>
